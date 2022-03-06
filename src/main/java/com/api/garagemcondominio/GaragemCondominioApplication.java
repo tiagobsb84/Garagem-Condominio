1272c8303716788e82ece5530bdfa676378f6d43
Tiago Silva <tiagobsb84@gmail.com>
2022-03-06 19:48:32 -0300

    Criando projeto

---
+++ src/main/java/com/api/garagemcondominio/GaragemCondominioApplication.java
@@ -0,0 +1,13 @@
+package com.api.garagemcondominio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaragemCondominioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaragemCondominioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/garagemcondominio/GaragemCondominioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.garagemcondominio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaragemCondominioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
